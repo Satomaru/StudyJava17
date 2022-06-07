26cb59d3e6ced0b4bcf1ef246cc9bec55dff773f
Satomaru <erynenadan@gmail.com>
2022-06-07 20:43:27 +0900

    Eitherで左右の判定をしていた部分を、LeftとRightにそれぞれ実装するように変更。

--- src/jp/satomaru/util/Either.java
+++ src/jp/satomaru/util/Either.java
@@ -23,9 +23,46 @@ public sealed interface Either<L, R> permits Left<L, R>, Right<L, R> {
 	 * @param <R> 右の型
 	 */
 	public record Left<L, R> (L value) implements Either<L, R> {
+
+		@Override
+		public boolean isLeft() {
+			return true;
+		}
+
 		@Override
 		public Optional<L> optional() {
 			return Optional.ofNullable(value);
+		}
+
+		@Override
+		public Optional<L> optionalLeft() {
+			return Optional.ofNullable(value);
+		}
+
+		@Override
+		public Optional<R> optionalRight() {
+			return Optional.empty();
+		}
+
+		@Override
+		public <E extends Exception> R orElseThrow(Function<L, E> generator) throws E {
+			throw generator.apply(value);
+		}
+
+		@Override
+		public <T> Optional<T> map(Function<? super L, T> whenLeft, Function<? super R, T> whenRight) {
+			return optional().map(whenLeft);
+		}
+
+		@Override
+		public Either<L, R> ifPresentLeft(Consumer<? super L> action) {
+			optional().ifPresent(action);
+			return this;
+		}
+
+		@Override
+		public Either<L, R> ifPresentRight(Consumer<? super R> action) {
+			return this;
 		}
 	}
 
@@ -36,9 +73,46 @@ public sealed interface Either<L, R> permits Left<L, R>, Right<L, R> {
 	 * @param <R> 右の型
 	 */
 	public record Right<L, R> (R value) implements Either<L, R> {
+
+		@Override
+		public boolean isLeft() {
+			return false;
+		}
+
 		@Override
 		public Optional<R> optional() {
 			return Optional.ofNullable(value);
+		}
+
+		@Override
+		public Optional<L> optionalLeft() {
+			return Optional.empty();
+		}
+
+		@Override
+		public Optional<R> optionalRight() {
+			return Optional.ofNullable(value);
+		}
+
+		@Override
+		public <E extends Exception> R orElseThrow(Function<L, E> generator) throws E {
+			return value;
+		}
+
+		@Override
+		public <T> Optional<T> map(Function<? super L, T> whenLeft, Function<? super R, T> whenRight) {
+			return optional().map(whenRight);
+		}
+
+		@Override
+		public Either<L, R> ifPresentLeft(Consumer<? super L> action) {
+			return this;
+		}
+
+		@Override
+		public Either<L, R> ifPresentRight(Consumer<? super R> action) {
+			optional().ifPresent(action);
+			return this;
 		}
 	}
 
@@ -50,6 +124,13 @@ public sealed interface Either<L, R> permits Left<L, R>, Right<L, R> {
 	Object value();
 
 	/**
+	 * 左の型を保持していることを判定します。
+	 *
+	 * @return 左の型を保持している場合はtrue
+	 */
+	boolean isLeft();
+
+	/**
 	 * 保持している値を取得します。
 	 *
 	 * @return 保持している値
@@ -57,48 +138,28 @@ public sealed interface Either<L, R> permits Left<L, R>, Right<L, R> {
 	Optional<?> optional();
 
 	/**
-	 * 左の型を保持していることを判定します。
-	 *
-	 * @return 左の型を保持している場合はtrue
-	 */
-	default boolean isLeft() {
-		return this instanceof Left;
-	}
-
-	/**
-	 * 右の型を保持していることを判定します。
-	 *
-	 * @return 右の型を保持している場合はtrue
-	 */
-	default boolean isRight() {
-		return this instanceof Right;
-	}
-
-	/**
 	 * 左の値を保持している場合、値を取得します。
 	 *
 	 * @return 保持している値
 	 */
-	default Optional<L> optionalLeft() {
-		if (this instanceof Left<L, R> left) {
-			return left.optional();
-		}
-
-		return Optional.empty();
-	}
+	Optional<L> optionalLeft();
 
 	/**
 	 * 右の値を保持している場合、値を取得します。
 	 *
 	 * @return 保持している値
 	 */
-	default Optional<R> optionalRight() {
-		if (this instanceof Right<L, R> right) {
-			return right.optional();
-		}
+	Optional<R> optionalRight();
 
-		return Optional.empty();
-	}
+	/**
+	 * 保持している値が、右の型の場合は返却し、左の型の場合はスローします。
+	 *
+	 * @param <E>       スローする例外
+	 * @param generator 左の型から例外を生成する関数
+	 * @return 右の値
+	 * @throws E 左の値を保持していた場合
+	 */
+	<E extends Exception> R orElseThrow(Function<L, E> generator) throws E;
 
 	/**
 	 * 保持している値を変換します。
@@ -108,16 +169,82 @@ public sealed interface Either<L, R> permits Left<L, R>, Right<L, R> {
 	 * @param whenRight 右の型を保持している場合に使用する関数
 	 * @return 変換後の値
 	 */
-	default <T> Optional<T> map(
-		Function<? super L, ? extends T> whenLeft,
-		Function<? super R, ? extends T> whenRight) {
+	<T> Optional<T> map(Function<? super L, T> whenLeft, Function<? super R, T> whenRight);
 
-		if (this instanceof Left<L, R> left) {
-			return left.optional().map(whenLeft);
-		}
+	/**
+	 * 左の値を保持していて、かつ値がnullではない場合、指定された関数を実行します。
+	 *
+	 * @param action 実行する関数
+	 * @return このオブジェクト自身
+	 */
+	Either<L, R> ifPresentLeft(Consumer<? super L> action);
 
-		Right<L, R> right = (Right<L, R>) this;
-		return right.optional().map(whenRight);
+	/**
+	 * 右の値を保持していて、かつ値がnullではない場合、指定された関数を実行します。
+	 *
+	 * @param action 実行する関数
+	 * @return このオブジェクト自身
+	 */
+	Either<L, R> ifPresentRight(Consumer<? super R> action);
+
+	/**
+	 * 右の型を保持していることを判定します。
+	 *
+	 * @return 右の型を保持している場合はtrue
+	 */
+	default boolean isRight() {
+		return !isLeft();
+	}
+
+	/**
+	 * 保持している値が、右の型の場合は返却し、左の型の場合は実行時例外をスローします。
+	 *
+	 * @param <E>              実行時例外
+	 * @param exceptionWrapper 左の型が例外だった場合に実行時例外を作成する関数
+	 * @param stringWrapper    左の型が例外ではなかった場合に実行時例外を作成する関数
+	 * @return 右の値
+	 */
+	default <E extends RuntimeException> R orElseThrowRuntime(
+		Function<Exception, E> exceptionWrapper,
+		Function<String, E> stringWrapper) {
+
+		return orElseThrow(leftValue -> {
+			if (leftValue == null) {
+				throw new NullPointerException();
+			}
+
+			if (leftValue instanceof RuntimeException runtime) {
+				throw runtime;
+			}
+
+			if (leftValue instanceof Exception exception) {
+				throw exceptionWrapper.apply(exception);
+			}
+
+			throw stringWrapper.apply(leftValue.toString());
+		});
+	}
+
+	/**
+	 * 保持している値が、右の型の場合は返却し、左の型の場合は不正引数例外をスローします。
+	 *
+	 * @return 右の値
+	 */
+	default R orElseIllegalArgument() {
+		return orElseThrowRuntime(
+			IllegalArgumentException::new,
+			IllegalArgumentException::new);
+	}
+
+	/**
+	 * 保持している値が、右の型の場合は返却し、左の型の場合は不正状態例外をスローします。
+	 *
+	 * @return 右の値
+	 */
+	default R orElseIllegalState() {
+		return orElseThrowRuntime(
+			IllegalStateException::new,
+			IllegalStateException::new);
 	}
 
 	/**
@@ -132,47 +259,10 @@ public sealed interface Either<L, R> permits Left<L, R>, Right<L, R> {
 	/**
 	 * 保持している値を文字列に編集します。
 	 *
-	 * @param whenLeft  左の型を保持している場合に使用するフォーマット
-	 * @param whenRight 右の型を保持している場合に使用するフォーマット
-	 * @return 文字列
-	 */
-	default Optional<String> format(String whenLeft, String whenRight) {
-		if (isLeft()) {
-			return optionalLeft().map(left -> String.format(whenLeft, left));
-		} else {
-			return optionalRight().map(right -> String.format(whenRight, right));
-		}
-	}
-
-	/**
-	 * 保持している値を文字列に編集します。
-	 *
 	 * @param format フォーマット
 	 * @return 文字列
 	 */
 	default Optional<String> format(String format) {
 		return optional().map(value -> String.format(format, value));
-	}
-
-	/**
-	 * 左の値を保持していて、かつ値がnullではない場合、指定された関数を実行します。
-	 *
-	 * @param action 実行する関数
-	 * @return このオブジェクト自身
-	 */
-	default Either<L, R> ifPresentLeft(Consumer<? super L> action) {
-		optionalLeft().ifPresent(action);
-		return this;
-	}
-
-	/**
-	 * 右の値を保持していて、かつ値がnullではない場合、指定された関数を実行します。
-	 *
-	 * @param action 実行する関数
-	 * @return このオブジェクト自身
-	 */
-	default Either<L, R> ifPresentRight(Consumer<? super R> action) {
-		optionalRight().ifPresent(action);
-		return this;
 	}
 }
